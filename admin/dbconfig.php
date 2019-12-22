8a003af12a15c46a565766bf1c3d47061ffc1d48
yevhen31546 <dobryanskiy.yevhen@gmail.com>
2019-12-22 07:45:50 +0200

    fix admin issue

--- admin/dbconfig.php
+++ admin/dbconfig.php
@@ -4,17 +4,17 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 define('BASE_PATH', dirname(__FILE__));
 
-define('BASE_URL', 'http://dev.myfamilybackccc.com:8081/admin/');
-define('DB_HOST', "localhost");
-define('DB_USER', "root");
-define('DB_PASSWORD', "");
-define('DB_NAME', "familymember");
+// define('BASE_URL', 'http://dev.myfamilybackccc.com:8081/admin/');
+// define('DB_HOST', "localhost");
+// define('DB_USER', "root");
+// define('DB_PASSWORD', "");
+// define('DB_NAME', "familymember");
 
-//define('BASE_URL', 'https://mynotes4u.com/admin/');
-//define('DB_HOST', "mysql.gip.superb.net");
-//define('DB_USER', "u_family_db");
-//define('DB_PASSWORD', "Norman@12345");
-//define('DB_NAME', "family_db");
+define('BASE_URL', 'https://mynotes4u.com/admin/');
+define('DB_HOST', "mysql.gip.superb.net");
+define('DB_USER', "u_family_db");
+define('DB_PASSWORD', "Norman@12345");
+define('DB_NAME', "family_db");
 
 define('APP_FOLDER', 'simpleadmin');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
