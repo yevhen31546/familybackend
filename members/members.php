65b4cc2fc82b7a36a49c4212d31da818b149eac4
yevhen31546 <dobryanskiy.yevhen@gmail.com>
2020-02-03 09:04:16 +0200

    update memeber.php

--- members/members.php
+++ members/members.php
@@ -27,7 +27,7 @@ $rows = $db->get('tbl_users');
             <div class="container">
                 <div class="row">
                     <!-- Main Content Start -->
-                    <div class="main--content col-md-8 pb--60" data-trigger="stickyScroll">
+                    <div class="main--content col-md-12 pb--60" data-trigger="stickyScroll">
                         <div class="main--content-inner">
                             <!-- Filter Nav Start -->
                             <div class="filter--nav pb--30 clearfix">
@@ -72,9 +72,7 @@ $rows = $db->get('tbl_users');
                                                     </h3>
                                                 </div>
 
-                                                <div class="activity">
-                                                    <p><i class="fa mr--8 fa-clock-o"></i>Active 5 monts ago</p>
-                                                </div>
+
 
                                                 <div class="actions">
                                                     <ul class="nav">
@@ -86,11 +84,6 @@ $rows = $db->get('tbl_users');
                                                         <li>
                                                             <a href="#" title="Add Friend" class="btn-link" data-toggle="tooltip" data-placement="bottom">
                                                                 <i class="fa fa-user-plus"></i>
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="#" title="Media" class="btn-link" data-toggle="tooltip" data-placement="bottom">
-                                                                <i class="fa fa-folder-o"></i>
                                                             </a>
                                                         </li>
                                                     </ul>
@@ -120,178 +113,7 @@ $rows = $db->get('tbl_users');
                     </div>
                     <!-- Main Content End -->
 
-                    <!-- Main Sidebar Start -->
-                    <div class="main--sidebar col-md-4 pb--60" data-trigger="stickyScroll">
-                        <!-- Widget Start -->
-                        <div class="widget">
-                            <h2 class="h4 fw--700 widget--title">Find A Buddy</h2>
 
-                            <!-- Buddy Finder Widget Start -->
-                            <div class="buddy-finder--widget">
-                                <form action="#">
-                                    <div class="row">
-                                        <div class="col-xs-6 col-xxs-12">
-                                            <div class="form-group">
-                                                <label>
-                                                    <span class="text-darker ff--primary fw--500">I Am</span>
-
-                                                    <select name="gender" class="form-control form-sm" data-trigger="selectmenu">
-                                                        <option value="male">Male</option>
-                                                        <option value="female">Female</option>
-                                                        <option value="other">Other</option>
-                                                    </select>
-                                                </label>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xs-6 col-xxs-12">
-                                            <div class="form-group">
-                                                <label>
-                                                    <span class="text-darker ff--primary fw--500">Looking For</span>
-
-                                                    <select name="lookingfor" class="form-control form-sm" data-trigger="selectmenu">
-                                                        <option value="female">Female</option>
-                                                        <option value="male">Male</option>
-                                                        <option value="other">Other</option>
-                                                    </select>
-                                                </label>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xs-6 col-xxs-12">
-                                            <div class="form-group">
-                                                <label>
-                                                    <span class="text-darker ff--primary fw--500">Age</span>
-
-                                                    <select name="age" class="form-control form-sm" data-trigger="selectmenu">
-                                                        <option value="18to25">18 to 25</option>
-                                                        <option value="25to30">25 to 30</option>
-                                                        <option value="30to35">30 to 35</option>
-                                                        <option value="35to40">35 to 40</option>
-                                                        <option value="40plus">40+</option>
-                                                    </select>
-                                                </label>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xs-6 col-xxs-12">
-                                            <div class="form-group">
-                                                <label>
-                                                    <span class="text-darker ff--primary fw--500">City</span>
-
-                                                    <select name="city" class="form-control form-sm" data-trigger="selectmenu">
-                                                        <option value="newyork">New York</option>
-                                                        <option value="California">California</option>
-                                                        <option value="Atlanta">Atlanta</option>
-                                                    </select>
-                                                </label>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xs-12">
-                                            <div class="form-group">
-                                                <label>
-                                                    <span class="text-darker ff--primary fw--500">Filter Country</span>
-
-                                                    <select name="city" class="form-control form-sm" data-trigger="selectmenu">
-                                                        <option value="unitedstates">United States</option>
-                                                        <option value="australia">Australia</option>
-                                                        <option value="turkey">Turkey</option>
-                                                        <option value="vietnam">Vietnam</option>
-                                                    </select>
-                                                </label>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xs-12">
-                                            <button type="submit" class="btn btn-primary">Search</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                            <!-- Buddy Finder Widget End -->
-                        </div>
-                        <!-- Widget End -->
-
-                        <!-- Widget Start -->
-                        <div class="widget">
-                            <h2 class="h4 fw--700 widget--title">Notice</h2>
-
-                            <!-- Text Widget Start -->
-                            <div class="text--widget">
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some  look even slightly believable.</p>
-                            </div>
-                            <!-- Text Widget End -->
-                        </div>
-                        <!-- Widget End -->
-
-                        <!-- Widget Start -->
-                        <div class="widget">
-                            <h2 class="h4 fw--700 widget--title">Forums</h2>
-
-                            <!-- Links Widget Start -->
-                            <div class="links--widget">
-                                <ul class="nav">
-                                    <li><a href="sub-forums.html">User Interface Design<span>(12)</span></a></li>
-                                    <li><a href="sub-forums.html">Front-End Engineering<span>(07)</span></a></li>
-                                    <li><a href="sub-forums.html">Web Development<span>(37)</span></a></li>
-                                    <li><a href="sub-forums.html">Social Media Marketing<span>(13)</span></a></li>
-                                    <li><a href="sub-forums.html">Content Marketing<span>(28)</span></a></li>
-                                </ul>
-                            </div>
-                            <!-- Links Widget End -->
-                        </div>
-                        <!-- Widget End -->
-
-                        <!-- Widget Start -->
-                        <div class="widget">
-                            <h2 class="h4 fw--700 widget--title">Archives</h2>
-
-                            <!-- Nav Widget Start -->
-                            <div class="nav--widget">
-                                <ul class="nav">
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-calendar-o"></i>
-                                            <span class="text">Jan - July 2017</span>
-                                            <span class="count">(86)</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-calendar-o"></i>
-                                            <span class="text">Jan - Dce 2016</span>
-                                            <span class="count">(328)</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-calendar-o"></i>
-                                            <span class="text">Jan - Dec 2015</span>
-                                            <span class="count">(427)</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <!-- Nav Widget End -->
-                        </div>
-                        <!-- Widget End -->
-
-                        <!-- Widget Start -->
-                        <div class="widget">
-                            <h2 class="h4 fw--700 widget--title">Advertisements</h2>
-
-                            <!-- Ad Widget Start -->
-                            <div class="ad--widget">
-                                <a href="#">
-                                    <img src="img/widgets-img/ad.jpg" alt="" class="center-block">
-                                </a>
-                            </div>
-                            <!-- Ad Widget End -->
-                        </div>
-                        <!-- Widget End -->
-                    </div>
-                    <!-- Main Sidebar End -->
                 </div>
             </div>
         </section>
